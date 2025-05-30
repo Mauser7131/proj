ce3f1bb6f7609cd648973dbda775c6f7dc3fc94e
Mauser7131 <pupil5_sia@school52.kiev.ua>
2025-05-30 13:20:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-function burgerMenuFn() {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const headerContainer = document.querySelector(".header__container");
-  const body = document.body;
-  burgerMenu.addEventListener("click", () => {
-    headerContainer.classList.toggle("header__max-height");
-    body.classList.toggle("no-scroll");
-  });
-}
-function heroModal() {
-  const a = document.querySelector(".hero__interaction-btn + div > a");
-  const modalOverlay = document.querySelector(".modal-overlay");
-  const modalWindow = document.querySelector(".modal-window");
-  const body = document.body;
-  const headerContainer = document.querySelector(".header__container");
+const burgerMenu = document.querySelector(".burger-menu");
+const headerContainer = document.querySelector(".header__container");
+const body = document.body;
+burgerMenu.addEventListener("click", () => {
+  headerContainer.classList.toggle("header__max-height");
+  body.classList.toggle("no-scroll");
+});
+const a = document.querySelector(".hero__interaction-btn + div > a");
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalWindow = document.querySelector(".modal-window");
 
-  a.addEventListener("click", (e) => {
-    e.preventDefault();
-    modalOverlay.style.display = "flex";
-    body.classList.toggle("no-scroll");
-    headerContainer.style.display = "none";
-  });
+a.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalOverlay.style.display = "flex";
+  body.classList.toggle("no-scroll");
+  headerContainer.style.display = "none";
+});
 
-  modalOverlay.addEventListener("click", () => {
-    modalOverlay.style.display = "none";
-    body.classList.toggle("no-scroll");
-    headerContainer.style.display = "block";
-  });
-}
-function cardsEfect() {
-  const cards = document.querySelectorAll(".card");
+modalOverlay.addEventListener("click", () => {
+  modalOverlay.style.display = "none";
+  body.classList.toggle("no-scroll");
+  headerContainer.style.display = "block";
+});
+const cards = document.querySelectorAll(".card");
 
-  cards.forEach((card, index) => {
-    card.addEventListener("click", () => {
-      card.classList.add("card__shadow");
-      document.querySelectorAll(".category__square").forEach((square) => {
-        square.classList.add("disable");
-      });
-      const square = card.querySelector(".category__square");
-      card.classList.remove("card__shadow");
-      square.classList.remove("disable");
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => {
+    card.classList.add("card__shadow");
+    document.querySelectorAll(".category__square").forEach((square) => {
+      square.classList.add("disable");
     });
+    const square = card.querySelector(".category__square");
+    card.classList.remove("card__shadow");
+    square.classList.remove("disable");
   });
-}
+});
